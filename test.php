8af64e4d247f20174d511cda10a3ef34b2af3f75
mashikos <mashikos@istyle.co.jp>
2017-04-18 18:25:38 +0900

    現在時刻を表示
    開発中メッセージを表示
    
    ちまちまコミット

--- test.php
+++ test.php
@@ -3,4 +3,6 @@
 
 $name = trim(fgets(STDIN));
 echo "Hello World!!";
-echo "ようこそ、$nameさん！";
+echo "ようこそ、$nameさん！". PHP_EOL;
+echo "今は". date("H:i:s"). "です。". PHP_EOL;
+echo "このプログラムは開発中です。". PHP_EOL;
