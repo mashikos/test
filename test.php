7bad68569ccfc3d0997c13d9c35baffcde95ce4f
mashikos <mashikos@istyle.co.jp>
2017-05-12 11:07:17 +0900

    簡易ログイン

--- test.php
+++ test.php
@@ -1,8 +1,17 @@
 <?php
 /* こんにちは！ */
 
-$name = trim(fgets(STDIN));
+echo "IDを入力して下さい";
+$id = trim(fgets(STDIN));
+echo "パスワードを入力して下さい";
+$password = trim(fgets(STDIN));
+
+if ($id != 'test' || $password != 'test') {
+	echo 'IDもしくはパスワードが違います。';
+	exit;
+}
+
 echo "Hello World!!";
-echo "ようこそ、$nameさん！". PHP_EOL;
+echo "ようこそ、$idさん！". PHP_EOL;
 echo "今は". date("H:i:s"). "です。". PHP_EOL;
 echo "このプログラムは開発中です。". PHP_EOL;
